aaa9e4c64bf90e4411e6cbb52991ced79cb85579
Pedro Henrique <phcs93@gmail.com>
2026-08-13 02:10:07 -0300

    small mistake from previous commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -56,10 +56,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     }
 
+    const currentUrl = window.location.href;
+
     // set current navbar link
     if (currentUrl !== "{domain}") {
-        const links = Array.from(document.querySelectorAll('div#navbar > a')).slice(1);
-        const currentUrl = window.location.href;
+        const links = Array.from(document.querySelectorAll('div#navbar > a')).slice(1);        
         const activeLink = links.find(link => link.href.split("/")[4] === currentUrl.split("/")[4]);
         if (activeLink) {
             activeLink.classList.add("active"); 
